5d9209970e5df1dfffc235475e4edc39e18f8287
Developer <unitygamesxojak@gmail.com>
2019-12-25 13:16:16 +0500

    basic-start-0.1v

---
+++ src/Components/app/app.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <header>
+        <div className="top">
+          <div className="logo">
+            <img src="images/logo" alt="This is art" />
+            <div>
+              <p>
+                <span></span>
+                <span></span>
+              </p>
+              <p></p>
+            </div>
+          </div>
+          <form>
+
+          </form>
+        </div>
+
+        <div className="banner">
+
+        </div>
+
+        <nav>
+
+        </nav>
+      </header>
+
+      <div class="content">
+
+      </div>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
